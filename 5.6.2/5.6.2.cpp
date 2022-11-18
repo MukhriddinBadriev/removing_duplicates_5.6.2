ab857c06ce892000e6a90c6c1eaf3a17e553a420
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-11-18 23:20:00 +0300

    Correction2

--- 5.6.2/5.6.2.cpp
+++ 5.6.2/5.6.2.cpp
@@ -6,23 +6,27 @@
 int main()
 {
     std::set<int> num;
-    std::vector<int>v = { 1,5,1,3,4,4 };
+    std::vector<int>v;
 
-    std::cout << "[IN]:" << '\n'<<v.size()<<'\n';   
-    
-    for (const auto& elem : v) {
-        std::cout << elem << '\n';
+    std::cout << "[IN]:" << '\n';
+    int size = 0;
+    std::cin >> size;
+
+    for (int i = 0; i < size; i++) {
+        int elem;
+        std::cin >> elem;
+        v.push_back(elem);
     }
 
-    for (int elem = 0; elem < v.size(); elem++) {
+    for (int elem = 0; elem < v.size(); elem++){
         num.insert(v[elem]);
     }
 
     auto r_i = num.rbegin();
     std::cout << "[OUT]:" << '\n';
-    while (r_i != num.rend()){
+    while (r_i != num.rend()) {
         std::cout << *r_i << std::endl;
-        r_i++;
+        r_i++;       
     }
 
     return 0;
